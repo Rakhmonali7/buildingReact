75d7eef0ab24f7cc197e733e95df052f944f7f2c
Rakhmonali7 <zamonalievr7@gmail.com>
2024-12-25 14:40:31 +0900

    dinamic button classes  Creating Flexible Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,49 @@
-import "./App.css";
-import Header from "./Header";
-import BodyOne from "./Body1";
-import Body2 from "./Body2";
+import React from 'react';
+import Button from './Button';
+import HomeIcon from './HomeIcon';
+import PlusIcon from './PlusIcon';
+import '../index.css';
 function App() {
   return (
-    <div>
-      <div className="navigationPanel">
-        <span className="logo">
-          <img src="/pic/logo.svg" alt="logo" />
-          Building
-        </span>
-        <nav>
-          <ul className="ul">
-            <li>Home</li>
-            <li>Service</li>
-            <li>About Us</li>
-            <li>Properties</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-        <button className="btnLogo">Log in</button>
-      </div>
-      <Header />
-      <BodyOne />
-      <Body2 />
+    <div id="app">
+      {/* default */}
+      <section>
+        <h2>Filled Button (Default)</h2>
+        <Button>Default</Button>
+        <Button mode="filled">Filled (Default)</Button>
+      </section>
+
+      {/* Button with Outline */}
+      <section>
+        <h2>Button with Outline</h2>
+        <Button mode="outline">Outline</Button>
+      </section>
+
+      {/* Text-only Button */}
+      <section>
+        <h2>Text-only Button</h2>
+        <Button mode="text">Text</Button>
+      </section>
+
+      {/* Button with Icon */}
+      <section>
+        <h2>Button with Icon</h2>
+        <Button Icon={HomeIcon}>Home</Button>
+        <Button Icon={PlusIcon} mode="text">
+          Add
+        </Button>
+      </section>
+
+      {/* Buttons Should Support Any Props */}
+      <section>
+        <h2>Buttons Should Support Any Props</h2>
+        <Button mode="filled" disabled>
+          Disabled
+        </Button>
+        <Button className={'hello'} onClick={() => console.log('Clicked!')}>
+          Click me
+        </Button>
+      </section>
     </div>
   );
 }
